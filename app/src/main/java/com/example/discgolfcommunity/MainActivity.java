3fef62d918b126deec22e65f081b30af35a52fe9
bersonconnor <bersonconnor@gmail.com>
2021-08-08 16:07:01 -0400

    Updated home menu view

--- app/src/main/java/com/example/discgolfcommunity/MainActivity.java
+++ app/src/main/java/com/example/discgolfcommunity/MainActivity.java
@@ -4,18 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button scoreCardButton;
+    private Button courseFinder;
+    private Button statisticsButton;
+    private Button helpButton;
+    private TextView welcomeTextView;
+
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // WELCOME BAR
+        userName = getUserName();
+        welcomeTextView = (TextView) findViewById(R.id.welcome_text);
+        welcomeTextView.setText("Welcome " + userName + "!");
+
+        // SCORE CARD BUTTON
         scoreCardButton = (Button) findViewById(R.id.score_card_button);
         scoreCardButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -23,5 +36,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // COURSE FINDER BUTTON
+        courseFinder = (Button) findViewById(R.id.course_finder_button);
+
+        // STATISTICS BUTTON
+        statisticsButton = (Button) findViewById(R.id.stat_button);
+
+        // HELP BUTTON
+        helpButton = (Button) findViewById(R.id.help_button);
     }
+
+    // TODO - get the real username once login features are implemented
+
+    /**
+     * Returns the user's username
+     *
+     * @return the user's username
+     */
+    private String getUserName() {
+        return "User";
+    }
+
 }
